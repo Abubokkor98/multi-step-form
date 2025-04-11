4dc1a7686ac79d5add091d076be55238595fcb41
Abubokkor98 <mail.abubokkor@gmail.com>
2025-04-11 23:10:24 +0600

    Add type definitions for PersonalInfo, AddressInfo, and AccountInfo

---
+++ src/types/formTypes.ts
@@ -0,0 +1,17 @@
+export type PersonalInfo = {
+  fullName: string;
+  email: string;
+  phone: string;
+};
+
+export type AddressInfo = {
+  street: string;
+  city: string;
+  zipCode: string;
+};
+
+export type AccountInfo = {
+  username: string;
+  password: string;
+  confirmPassword: string;
+};
